4f16c24e71fd612b2d033f60c33188f91105e820
Raghavyd <kumaraman96450@gmail.com>
2026-02-21 09:29:04 +0530

    final vercel fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // critical for Vercel
+  base: "/", // important
   build: {
     outDir: "dist",
     emptyOutDir: true
